177efbed4a1ad38dd14089165898458a10657101
Er-Rahul-Tiwari <rahultiwari01071996@gmail.com>
2022-03-15 08:48:24 +0530

    proxy middlware

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const path = require("path")
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const app = express()
-
-app.use(express.static(path.join(__dirname, "build")))
-
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"))
-})
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
-
-app.listen(process.env.PORT || 8080)
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:5000',
+      changeOrigin: true,
+    })
+  );
+};
